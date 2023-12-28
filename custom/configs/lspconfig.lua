8cc96c8fa43e3501614b21509847531e72ad2d25
Ahmed <ahmedelsharawy117@gmail.com>
2023-12-28 21:54:49 +0200

    Modified lspconfig

--- custom/configs/lspconfig.lua
+++ custom/configs/lspconfig.lua
@@ -1,20 +1,20 @@
-local base = require "plugins.configs.lspconfig"
+local base = require("plugins.configs.lspconfig")
 local on_attach = base.on_attach
 local capabilities = base.capabilities
 
-local lspconfig = require "lspconfig"
+local lspconfig = require("lspconfig")
 
-local servers = { "tsserver", "tailwindcss", "eslint", "gopls", "pyright" }
+local servers = { "tsserver", "tailwindcss", "eslint", "gopls", "pyright", "clangd" }
 
 for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = on_attach,
-    capabilities = capabilities,
-  }
+	lspconfig[lsp].setup({
+		on_attach = on_attach,
+		capabilities = capabilities,
+	})
 end
 
-lspconfig.rust_analyzer.setup {
-  on_attach = on_attach,
-  capabilities = capabilities,
-  filetypes = { "rust" },
-}
+lspconfig.rust_analyzer.setup({
+	on_attach = on_attach,
+	capabilities = capabilities,
+	filetypes = { "rust" },
+})
